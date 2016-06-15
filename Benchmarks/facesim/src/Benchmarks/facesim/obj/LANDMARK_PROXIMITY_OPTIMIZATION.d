f1f23b1cf4ff6a679e6c4afcf596b4d65e21dea4
Vladimir Janjic <vj32@st-andrews.ac.uk>
2016-06-15 14:48:26 +0100

    added facesim

---
+++ Benchmarks/facesim/src/Benchmarks/facesim/obj/LANDMARK_PROXIMITY_OPTIMIZATION.d
@@ -0,0 +1,309 @@
+obj/LANDMARK_PROXIMITY_OPTIMIZATION.o \
+  obj/LANDMARK_PROXIMITY_OPTIMIZATION.d: \
+  LANDMARK_PROXIMITY_OPTIMIZATION.cpp LANDMARK_PROXIMITY_OPTIMIZATION.h \
+  ../../Public_Library/Matrices_And_Vectors/MATRIX_MXN.h \
+  ../../Public_Library/Matrices_And_Vectors/VECTOR_ND.h \
+  ../../Public_Library/Matrices_And_Vectors/../Math_Tools/sqr.h \
+  ../../Public_Library/Matrices_And_Vectors/../Math_Tools/maxabs.h \
+  ../../Public_Library/Matrices_And_Vectors/../Math_Tools/max.h \
+  ../../Public_Library/Matrices_And_Vectors/../Read_Write/READ_WRITE_FUNCTIONS.h \
+  ../../Public_Library/Matrices_And_Vectors/MATRIX_NXN.h \
+  ../../Public_Library/Matrices_And_Vectors/../Math_Tools/exchange.h \
+  FACE_OPTIMIZATION.h \
+  ../../Public_Library/Deformable_Objects/DEFORMABLE_OBJECT_3D.h \
+  ../../Public_Library/Deformable_Objects/DEFORMABLE_OBJECT.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/LIST_ARRAY.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/ARRAY.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Matrices_And_Vectors/VECTOR_2D.h \
+  ../../Public_Library/Matrices_And_Vectors/../Math_Tools/min.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Math_Tools/maxmag.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Math_Tools/minmag.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Utilities/TYPE_UTILITIES.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Utilities/STATIC_ASSERT.h \
+  ../../Public_Library/Deformable_Objects/../Particles/SOLIDS_PARTICLE.h \
+  ../../Public_Library/Deformable_Objects/../Particles/PARTICLE.h \
+  ../../Public_Library/Deformable_Objects/../Particles/../Read_Write/FILE_UTILITIES.h \
+  ../../Public_Library/Matrices_And_Vectors/../Read_Write/../Utilities/STRING_UTILITIES.h \
+  ../../Public_Library/Deformable_Objects/../Particles/PARTICLE_ATTRIBUTE.h \
+  ../../Public_Library/Deformable_Objects/../Particles/PARTICLE_ATTRIBUTE_UNTEMPLATIZED.h \
+  ../../Public_Library/Deformable_Objects/../Particles/PARTICLE_POSITION_ATTRIBUTE.h \
+  ../../Public_Library/Deformable_Objects/../Particles/../Interpolation/LINEAR_INTERPOLATION.h \
+  ../../Public_Library/Deformable_Objects/../Particles/../Interpolation/INTERPOLATION.h \
+  ../../Public_Library/Deformable_Objects/../Particles/../Interpolation/../Arrays/ARRAYS_1D.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Math_Tools/clamp.h \
+  ../../Public_Library/Matrices_And_Vectors/../Math_Tools/../Matrices_And_Vectors/VECTOR_1D.h \
+  ../../Public_Library/Matrices_And_Vectors/../Math_Tools/../Matrices_And_Vectors/VECTOR_3D.h \
+  ../../Public_Library/Matrices_And_Vectors/../Math_Tools/constants.h \
+  ../../Public_Library/Matrices_And_Vectors/../Math_Tools/../Matrices_And_Vectors/SYMMETRIC_MATRIX_2X2.h \
+  ../../Public_Library/Matrices_And_Vectors/MATRIX_2X2.h \
+  ../../Public_Library/Matrices_And_Vectors/SYMMETRIC_MATRIX_2X2.h \
+  ../../Public_Library/Matrices_And_Vectors/UPPER_TRIANGULAR_MATRIX_2X2.h \
+  ../../Public_Library/Matrices_And_Vectors/DIAGONAL_MATRIX_2X2.h \
+  ../../Public_Library/Matrices_And_Vectors/../Math_Tools/sign.h \
+  ../../Public_Library/Matrices_And_Vectors/../Math_Tools/../Matrices_And_Vectors/SYMMETRIC_MATRIX_3X3.h \
+  ../../Public_Library/Matrices_And_Vectors/MATRIX_3X2.h \
+  ../../Public_Library/Matrices_And_Vectors/MATRIX_3X3.h \
+  ../../Public_Library/Matrices_And_Vectors/UPPER_TRIANGULAR_MATRIX_3X3.h \
+  ../../Public_Library/Matrices_And_Vectors/DIAGONAL_MATRIX_3X3.h \
+  ../../Public_Library/Matrices_And_Vectors/SYMMETRIC_MATRIX_3X3.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/GRID_1D.h \
+  ../../Public_Library/Deformable_Objects/../Particles/../Interpolation/../Arrays/ARRAYS_2D.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/GRID_2D.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/BOX_2D.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Geometry/SEGMENT_2D.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Geometry/RAY_2D.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Geometry/BOX_2D.h \
+  ../../Public_Library/Deformable_Objects/../Particles/../Interpolation/../Arrays/ARRAYS_3D.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/GRID_3D.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/BOX_3D.h \
+  ../../Public_Library/Deformable_Objects/../Particles/PARTICLE_VELOCITY_ATTRIBUTE.h \
+  ../../Public_Library/Deformable_Objects/../Particles/PARTICLE_MASS_ATTRIBUTE.h \
+  ../../Public_Library/Deformable_Objects/../Particles/PARTICLE_ATTRIBUTE_COLLECTION_MAP.h \
+  ../../Public_Library/Deformable_Objects/../Particles/PARTICLE_ATTRIBUTE_COLLECTION.h \
+  ../../Public_Library/Deformable_Objects/../Forces_And_Torques/EXTERNAL_FORCES_AND_VELOCITIES.h \
+  ../../Public_Library/Deformable_Objects/../Forces_And_Torques/../Utilities/DEBUG_UTILITIES.h \
+  ../../Public_Library/Deformable_Objects/DEFORMABLE_OBJECT_COLLISIONS_3D.h \
+  ../../Public_Library/Deformable_Objects/DEFORMABLE_OBJECT_COLLISIONS.h \
+  ../../Public_Library/Deformable_Objects/../Data_Structures/PAIR.h \
+  ../../Public_Library/Deformable_Objects/../Geometry/SEGMENTED_CURVE_3D.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/SEGMENT_3D.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Grids/SEGMENT_MESH.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Arrays/LIST_ARRAYS.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/ARRAYS.h \
+  ../../Public_Library/Deformable_Objects/../Geometry/TRIANGULATED_SURFACE.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/TRIANGULATED_OBJECT.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Grids/TRIANGLE_MESH.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/TRIANGLE_3D.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/PLANE.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Collisions_And_Interactions/PARTICLE_PARTITION.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Collisions_And_Interactions/../Geometry/IMPLICIT_SURFACE.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/RAY_3D.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Level_Sets/LEVELSET.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Level_Sets/../Geometry/POLYGON.h \
+  ../../Public_Library/Deformable_Objects/../Collisions_And_Interactions/COLLISION_BODY_3D.h \
+  ../../Public_Library/Deformable_Objects/../Fracture/EMBEDDED_TETRAHEDRALIZED_VOLUME_BOUNDARY_SURFACE.h \
+  ../../Public_Library/Deformable_Objects/../Fracture/../Geometry/EMBEDDED_TETRAHEDRALIZED_VOLUME.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/EMBEDDED_OBJECT.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Data_Structures/HASHTABLE_2D.h \
+  ../../Public_Library/Deformable_Objects/../Data_Structures/../Math_Tools/hash_function.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/TETRAHEDRALIZED_VOLUME.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/TETRAHEDRON.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Collisions_And_Interactions/TETRAHEDRON_HIERARCHY.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Collisions_And_Interactions/BOX_HIERARCHY.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Collisions_And_Interactions/../Geometry/ORIENTED_BOX_3D.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Matrices_And_Vectors/QUATERNION.h \
+  ../../Public_Library/Matrices_And_Vectors/MATRIX_4X4.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Collisions_And_Interactions/../Data_Structures/STACK.h \
+  ../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Collisions_And_Interactions/../Grids/TETRAHEDRON_MESH.h \
+  ../../Public_Library/Deformable_Objects/../Fracture/../Data_Structures/HASHTABLE_3D.h \
+  ../../Public_Library/Forces_And_Torques/SOLIDS_FORCES.h \
+  ../../Public_Library/Solids_And_Fluids/SOLIDS_EVOLUTION_CALLBACKS.h \
+  FACE_CONTROL_PARAMETERS.h FACE_CONTROL_SET.h \
+  FACE_LANDMARK_OPTIMIZATION_GOAL.h FACE_OPTIMIZATION_GOAL.h \
+  LANDMARK_3D.h
+
+LANDMARK_PROXIMITY_OPTIMIZATION.h:
+
+../../Public_Library/Matrices_And_Vectors/MATRIX_MXN.h:
+
+../../Public_Library/Matrices_And_Vectors/VECTOR_ND.h:
+
+../../Public_Library/Matrices_And_Vectors/../Math_Tools/sqr.h:
+
+../../Public_Library/Matrices_And_Vectors/../Math_Tools/maxabs.h:
+
+../../Public_Library/Matrices_And_Vectors/../Math_Tools/max.h:
+
+../../Public_Library/Matrices_And_Vectors/../Read_Write/READ_WRITE_FUNCTIONS.h:
+
+../../Public_Library/Matrices_And_Vectors/MATRIX_NXN.h:
+
+../../Public_Library/Matrices_And_Vectors/../Math_Tools/exchange.h:
+
+FACE_OPTIMIZATION.h:
+
+../../Public_Library/Deformable_Objects/DEFORMABLE_OBJECT_3D.h:
+
+../../Public_Library/Deformable_Objects/DEFORMABLE_OBJECT.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/LIST_ARRAY.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/ARRAY.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Matrices_And_Vectors/VECTOR_2D.h:
+
+../../Public_Library/Matrices_And_Vectors/../Math_Tools/min.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Math_Tools/maxmag.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Math_Tools/minmag.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Utilities/TYPE_UTILITIES.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Utilities/STATIC_ASSERT.h:
+
+../../Public_Library/Deformable_Objects/../Particles/SOLIDS_PARTICLE.h:
+
+../../Public_Library/Deformable_Objects/../Particles/PARTICLE.h:
+
+../../Public_Library/Deformable_Objects/../Particles/../Read_Write/FILE_UTILITIES.h:
+
+../../Public_Library/Matrices_And_Vectors/../Read_Write/../Utilities/STRING_UTILITIES.h:
+
+../../Public_Library/Deformable_Objects/../Particles/PARTICLE_ATTRIBUTE.h:
+
+../../Public_Library/Deformable_Objects/../Particles/PARTICLE_ATTRIBUTE_UNTEMPLATIZED.h:
+
+../../Public_Library/Deformable_Objects/../Particles/PARTICLE_POSITION_ATTRIBUTE.h:
+
+../../Public_Library/Deformable_Objects/../Particles/../Interpolation/LINEAR_INTERPOLATION.h:
+
+../../Public_Library/Deformable_Objects/../Particles/../Interpolation/INTERPOLATION.h:
+
+../../Public_Library/Deformable_Objects/../Particles/../Interpolation/../Arrays/ARRAYS_1D.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Math_Tools/clamp.h:
+
+../../Public_Library/Matrices_And_Vectors/../Math_Tools/../Matrices_And_Vectors/VECTOR_1D.h:
+
+../../Public_Library/Matrices_And_Vectors/../Math_Tools/../Matrices_And_Vectors/VECTOR_3D.h:
+
+../../Public_Library/Matrices_And_Vectors/../Math_Tools/constants.h:
+
+../../Public_Library/Matrices_And_Vectors/../Math_Tools/../Matrices_And_Vectors/SYMMETRIC_MATRIX_2X2.h:
+
+../../Public_Library/Matrices_And_Vectors/MATRIX_2X2.h:
+
+../../Public_Library/Matrices_And_Vectors/SYMMETRIC_MATRIX_2X2.h:
+
+../../Public_Library/Matrices_And_Vectors/UPPER_TRIANGULAR_MATRIX_2X2.h:
+
+../../Public_Library/Matrices_And_Vectors/DIAGONAL_MATRIX_2X2.h:
+
+../../Public_Library/Matrices_And_Vectors/../Math_Tools/sign.h:
+
+../../Public_Library/Matrices_And_Vectors/../Math_Tools/../Matrices_And_Vectors/SYMMETRIC_MATRIX_3X3.h:
+
+../../Public_Library/Matrices_And_Vectors/MATRIX_3X2.h:
+
+../../Public_Library/Matrices_And_Vectors/MATRIX_3X3.h:
+
+../../Public_Library/Matrices_And_Vectors/UPPER_TRIANGULAR_MATRIX_3X3.h:
+
+../../Public_Library/Matrices_And_Vectors/DIAGONAL_MATRIX_3X3.h:
+
+../../Public_Library/Matrices_And_Vectors/SYMMETRIC_MATRIX_3X3.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/GRID_1D.h:
+
+../../Public_Library/Deformable_Objects/../Particles/../Interpolation/../Arrays/ARRAYS_2D.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/GRID_2D.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/BOX_2D.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Geometry/SEGMENT_2D.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Geometry/RAY_2D.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Geometry/BOX_2D.h:
+
+../../Public_Library/Deformable_Objects/../Particles/../Interpolation/../Arrays/ARRAYS_3D.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/GRID_3D.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/BOX_3D.h:
+
+../../Public_Library/Deformable_Objects/../Particles/PARTICLE_VELOCITY_ATTRIBUTE.h:
+
+../../Public_Library/Deformable_Objects/../Particles/PARTICLE_MASS_ATTRIBUTE.h:
+
+../../Public_Library/Deformable_Objects/../Particles/PARTICLE_ATTRIBUTE_COLLECTION_MAP.h:
+
+../../Public_Library/Deformable_Objects/../Particles/PARTICLE_ATTRIBUTE_COLLECTION.h:
+
+../../Public_Library/Deformable_Objects/../Forces_And_Torques/EXTERNAL_FORCES_AND_VELOCITIES.h:
+
+../../Public_Library/Deformable_Objects/../Forces_And_Torques/../Utilities/DEBUG_UTILITIES.h:
+
+../../Public_Library/Deformable_Objects/DEFORMABLE_OBJECT_COLLISIONS_3D.h:
+
+../../Public_Library/Deformable_Objects/DEFORMABLE_OBJECT_COLLISIONS.h:
+
+../../Public_Library/Deformable_Objects/../Data_Structures/PAIR.h:
+
+../../Public_Library/Deformable_Objects/../Geometry/SEGMENTED_CURVE_3D.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/SEGMENT_3D.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Grids/SEGMENT_MESH.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Arrays/LIST_ARRAYS.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/ARRAYS.h:
+
+../../Public_Library/Deformable_Objects/../Geometry/TRIANGULATED_SURFACE.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/TRIANGULATED_OBJECT.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Grids/TRIANGLE_MESH.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/TRIANGLE_3D.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/PLANE.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Collisions_And_Interactions/PARTICLE_PARTITION.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Collisions_And_Interactions/../Geometry/IMPLICIT_SURFACE.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/RAY_3D.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Level_Sets/LEVELSET.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Level_Sets/../Geometry/POLYGON.h:
+
+../../Public_Library/Deformable_Objects/../Collisions_And_Interactions/COLLISION_BODY_3D.h:
+
+../../Public_Library/Deformable_Objects/../Fracture/EMBEDDED_TETRAHEDRALIZED_VOLUME_BOUNDARY_SURFACE.h:
+
+../../Public_Library/Deformable_Objects/../Fracture/../Geometry/EMBEDDED_TETRAHEDRALIZED_VOLUME.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/EMBEDDED_OBJECT.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Data_Structures/HASHTABLE_2D.h:
+
+../../Public_Library/Deformable_Objects/../Data_Structures/../Math_Tools/hash_function.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/TETRAHEDRALIZED_VOLUME.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/TETRAHEDRON.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Collisions_And_Interactions/TETRAHEDRON_HIERARCHY.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Collisions_And_Interactions/BOX_HIERARCHY.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Collisions_And_Interactions/../Geometry/ORIENTED_BOX_3D.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Matrices_And_Vectors/QUATERNION.h:
+
+../../Public_Library/Matrices_And_Vectors/MATRIX_4X4.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Collisions_And_Interactions/../Data_Structures/STACK.h:
+
+../../Public_Library/Deformable_Objects/../Arrays/../Grids/../Geometry/../Collisions_And_Interactions/../Grids/TETRAHEDRON_MESH.h:
+
+../../Public_Library/Deformable_Objects/../Fracture/../Data_Structures/HASHTABLE_3D.h:
+
+../../Public_Library/Forces_And_Torques/SOLIDS_FORCES.h:
+
+../../Public_Library/Solids_And_Fluids/SOLIDS_EVOLUTION_CALLBACKS.h:
+
+FACE_CONTROL_PARAMETERS.h:
+
+FACE_CONTROL_SET.h:
+
+FACE_LANDMARK_OPTIMIZATION_GOAL.h:
+
+FACE_OPTIMIZATION_GOAL.h:
+
+LANDMARK_3D.h:
